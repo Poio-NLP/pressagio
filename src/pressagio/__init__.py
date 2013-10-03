77b3f1d1f6eca0067aa44dfe380136e4cdfc3669
Peter Bouda <pbouda@cidles.eu>
2013-10-03 17:53:04 +0100

    Added Pressagio class for easy predictions.

--- src/pressagio/__init__.py
+++ src/pressagio/__init__.py
@@ -6,3 +6,29 @@
 # Author: Peter Bouda <pbouda@cidles.eu>
 # URL: <http://media.cidles.eu/poio/>
 # For license information, see LICENSE
+
+import configparser
+
+import pressagio.predictor
+import pressagio.context_tracker
+
+class Pressagio:
+
+    def __init__(self, callback, config_file):
+        self.config = configparser.ConfigParser()
+        self.config.read(config_file)
+        self.callback = callback
+
+        self.predictor_registry = pressagio.predictor.PredictorRegistry(
+            self.config)
+        self.context_tracker = pressagio.context_tracker.ContextTracker(
+            self.config, self.predictor_registry, callback)
+
+        self.predictor_activator = pressagio.predictor.PredictorActivator(
+            self.config, self.predictor_registry, self.context_tracker)
+        self.predictor_activator.combination_policy = "meritocracy"
+
+    def predict(self):
+        multiplier = 1
+        prediction = self.predictor_activator.predict(multiplier)
+        return prediction
