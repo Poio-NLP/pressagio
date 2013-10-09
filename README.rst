8b1197ec914e417bfd3078bf6586f1ca8d730c3b
Peter Bouda <pbouda@cidles.eu>
2013-10-09 12:17:43 +0100

    Updated setup.py and README text.

---
+++ README.rst
@@ -0,0 +1,16 @@
+Pressagio text prediction system
+================================
+Pressagio is a library that predicts text based on n-gram models. For example,
+you can send a string and the library will return the most likely word
+completions for the last token in the string.
+
+Pressagio is a pure Python port of the presage library:
+
+http://presage.sourceforge.net/
+
+License
+-------
+Pressagio source code is distributed under the Apache 2.0 License.
+
+Pressagio documentation is distributed under the Creative Commons Attribution
+3.0 Unported.
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 #
 # Copyright (C) 2001-2013 Poio Project
 # Author: Peter Bouda <pbouda@cidles.eu>
-# URL: <http://www.cidles.eu/ltll/poio>
+# URL: <http://media.cidles.eu/poio/>
 # For license information, see LICENSE
 
 import os
@@ -14,8 +14,8 @@ version_file = os.path.join(os.path.dirname(__file__), 'src', 'pressagio', 'VERS
 with open(version_file) as fh:
     pressagio_version = fh.read().strip()
 
-import distribute_setup
-distribute_setup.use_setuptools()
+#import distribute_setup
+#distribute_setup.use_setuptools()
 
 from setuptools import setup, find_packages
 
@@ -24,8 +24,8 @@ setup(
     description = "A Python Library for statistical text prediction.",
     version = pressagio_version,
     url = "https://github.com/cidles/pressagio",
-    #download_url = "http://media.cidles.eu/poio/poio-api/",
-    long_description = "Pressagio is a library that predicts text based on statistical n-gram models. For example, you can send a string and the library will return the most likely word completions for the last token in the string.",
+    download_url = "https://s3.amazonaws.com/cidles/downloads/pressagio/pressagio-0.1.0.zip",
+    long_description = "Pressagio is a library that predicts text based on n-gram models. For example, you can send a string and the library will return the most likely word completions for the last token in the string.",
     license = "Apache License, Version 2.0",
     keywords = ['NLP', 'CL', 'natural language processing',
                 'computational linguistics', 'parsing', 'tagging',
