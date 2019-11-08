1388679c8ac18bf1eddb44f4f36063048478e5bd
Peter Bouda <hey@peterbouda.eu>
2019-11-08 16:02:48 +0100

    Version bump.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ URL = "https://github.com/Poio-NLP/pressagio"
 EMAIL = "pbouda@outlook.com"
 AUTHOR = "Peter Bouda"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "0.1.4"
+VERSION = "0.1.5"
 
 # What packages are required for this module to be executed?
 REQUIRED = ["cymem", "preshed", "murmurhash", "cython"]
@@ -150,7 +150,7 @@ def clean(path):
 
 def setup_package():
     if len(sys.argv) > 1 and sys.argv[1] == "clean":
-        return clean(root)
+        return clean(here)
 
     include_dirs = [get_python_inc(plat_specific=True)]
     if (
